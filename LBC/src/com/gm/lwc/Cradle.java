09c07c07d1c995866f26fc82bd7a495a33fa6f79
Gabriel Seralha Miró <gsmiro@gmail.com>
2013-11-20 21:42:15 -0200

    Implemented Add and Sub

--- LBC/src/com/gm/lwc/Cradle.java
+++ LBC/src/com/gm/lwc/Cradle.java
@@ -21,7 +21,7 @@ public class Cradle {
 	}
 
 	public static enum Cons {
-		TAB(9), LF(10);
+		TAB(9), PLUS(43), MINUS(45);
 
 		private int v;
 
@@ -43,25 +43,29 @@ public class Cradle {
 	private PrintWriter err;
 	private int look;
 
-	public void getChar() throws IOException {
-		look = in.read();
+	public void getChar() {
+		try {
+			look = in.read();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	public void error(String m) throws IOException {
+	public void error(String m) {
 		err.write(m);
 		err.flush();
 	}
 
-	public void abort(String s) throws IOException {
+	public void abort(String s) {
 		error(s);
 		System.exit(-1);
 	}
 
-	public void expected(String s) throws IOException {
+	public void expected(String s) {
 		abort("Expected " + s);
 	}
 
-	public void match(int chr) throws IOException {
+	public void match(int chr) {
 		if (look == chr)
 			getChar();
 		else
@@ -76,7 +80,7 @@ public class Cradle {
 		return c >= 48 && c <= 57;
 	}
 
-	public int getName() throws IOException {
+	public int getName() {
 		if (!isAlpha(look))
 			expected("Name");
 		int l = look;
@@ -84,7 +88,7 @@ public class Cradle {
 		return l;
 	}
 
-	public int getNum() throws IOException {
+	public int getNum() {
 		if (!isDigit(look))
 			expected("Integer");
 		int l = look;
@@ -92,34 +96,61 @@ public class Cradle {
 		return l;
 	}
 
-	public String emit(String s) throws IOException {
+	public String emit(String s) {
 		s = Cons.TAB.str() + s;
 		out.print(s);
 		out.flush();
 		return s;
 	}
 
-	public String emitln(String s) throws IOException {
+	public String emitln(String s) {
 		s = emit(s);
 		out.println();
 		return s;
 	}
 
-	public void init() throws IOException {
+	public void init() {
 		getChar();
 	}
 
-	public void expression() throws IOException {
+	public void term() {
 		emitln("MOVE #" + getNum() + ", D0");
 	}
 
+	public void add() {
+		match(Cons.PLUS.val());
+		term();
+		emitln("ADD D1,D0");
+	}
+
+	public void sub() {
+		match(Cons.MINUS.val());
+		term();
+		emitln("SUB D1,D0");
+		emitln("NEG D0");
+	}
+
+	public void expression() {
+		term();
+		emitln("MOVE D0,D1");
+		if (look == Cons.PLUS.val())
+			add();
+
+		else if (look == Cons.MINUS.val())
+			sub();
+
+		else
+			expected("Addop");
+	}
+
 	public static void main(String[] args) {
-		try {
-			Cradle c = new Cradle();
-			c.init();
-			c.expression();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		while (true)
+			try {
+				Cradle c = new Cradle();
+				c.init();
+				c.expression();
+			} catch (RuntimeException e) {
+				e.printStackTrace();
+			}
 	}
 }
